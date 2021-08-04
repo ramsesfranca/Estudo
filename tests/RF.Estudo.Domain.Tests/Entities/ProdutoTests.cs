7dc31a25c3a87689eb63bcd915d192ffd946dfc4
Ramsés França <ramsesfranca@hotmail.com>
2021-08-04 08:34:51 -0300

    Correções.

--- tests/RF.Estudo.Domain.Tests/Entities/ProdutoTests.cs
+++ tests/RF.Estudo.Domain.Tests/Entities/ProdutoTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using RF.Estudo.Domain.Core.Exceptions;
+using RF.Estudo.Domain.Entities;
+using RF.Estudo.Domain.ValueObjects;
+using Xunit;
 
 namespace RF.Estudo.Domain.Tests
 {
@@ -9,41 +12,35 @@ namespace RF.Estudo.Domain.Tests
         {
             // Arrange & Act & Assert
 
-            //var ex = Assert.Throws<DomainException>(() =>
-            //    new Produto(string.Empty, "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
-            //);
+            var ex = Assert.Throws<DomainException>(() =>
+                new Produto(string.Empty, "Jogue o melhor e mais vendido game de corrida desta geração.", 100, new Dimensoes(1, 1, 1), false, new Categoria("Alimentos", "Alimentos"))
+            );
 
-            //Assert.Equal("O campo Nome do produto não pode estar vazio", ex.Message);
+            Assert.Equal("O campo Nome do produto não pode estar vazio", ex.Message);
 
-            //ex = Assert.Throws<DomainException>(() =>
-            //    new Produto("Nome", string.Empty, false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
-            //);
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Console Xbox One S 1TB", string.Empty, 100, new Dimensoes(1, 1, 1), false, new Categoria("Alimentos", "Alimentos"))
+            );
 
-            //Assert.Equal("O campo Descricao do produto não pode estar vazio", ex.Message);
+            Assert.Equal("O campo Descricao do produto não pode estar vazio", ex.Message);
 
-            //ex = Assert.Throws<DomainException>(() =>
-            //    new Produto("Nome", "Descricao", false, 0, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
-            //);
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Console Xbox One S 1TB", "Jogue o melhor e mais vendido game de corrida desta geração.", 0, new Dimensoes(1, 1, 1), false, new Categoria("Alimentos", "Alimentos"))
+            );
 
-            //Assert.Equal("O campo Valor do produto não pode se menor igual a 0", ex.Message);
-
-            //ex = Assert.Throws<DomainException>(() =>
-            //    new Produto("Nome", "Descricao", false, 100, Guid.Empty, DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
-            //);
-
-            //Assert.Equal("O campo CategoriaId do produto não pode estar vazio", ex.Message);
+            Assert.Equal("O campo Valor do produto não pode se menor igual a 0", ex.Message);
 
             //ex = Assert.Throws<DomainException>(() =>
-            //    new Produto("Nome", "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, string.Empty, new Dimensoes(1, 1, 1))
+            //    new Produto("Console Xbox One S 1TB", "Jogue o melhor e mais vendido game de corrida desta geração.", 100, new Dimensoes(1, 1, 1), false, null)
             //);
 
-            //Assert.Equal("O campo Imagem do produto não pode estar vazio", ex.Message);
+            //Assert.Equal("A Categoria do produto não pode estar vazio", ex.Message);
 
-            //ex = Assert.Throws<DomainException>(() =>
-            //    new Produto("Nome", "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(0, 1, 1))
-            //);
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Console Xbox One S 1TB", "Jogue o melhor e mais vendido game de corrida desta geração.", 100, new Dimensoes(0, 1, 1), false, new Categoria("Alimentos", "Alimentos"))
+            );
 
-            //Assert.Equal("O campo Altura não pode ser menor ou igual a 0", ex.Message);
+            Assert.Equal("O campo Altura não pode ser menor ou igual a 0", ex.Message);
         }
     }
 }
