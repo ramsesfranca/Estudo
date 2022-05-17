df7d1cfee8ea17f46546d3afb4457412b3b8fed0
Ramsés França <ramsesfranca@hotmail.com>
2022-05-17 13:37:47 -0300

    Modificações.

--- src/RF.Estudo.Application/ViewModels/Categoria/FormularioCategoriaViewModel.cs
+++ src/RF.Estudo.Application/ViewModels/Categoria/FormularioCategoriaViewModel.cs
@@ -5,9 +5,11 @@ namespace RF.Estudo.Application.ViewModels.Categoria
     public class FormularioCategoriaViewModel : BaseViewModel
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(250, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(500, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Descricao { get; set; }
     }
 }
--- src/RF.Estudo.Application/ViewModels/Produto/FormularioProdutoViewModel.cs
+++ src/RF.Estudo.Application/ViewModels/Produto/FormularioProdutoViewModel.cs
@@ -11,9 +11,11 @@ namespace RF.Estudo.Application.ViewModels.Produto
         public Guid CategoriaId { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(250, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(500, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Descricao { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
